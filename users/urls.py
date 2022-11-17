3707c26b355a16cddd3c5e137aad6dd9e1f568a3
Dilbar <auybova.dilbar@gmail.com>
2022-11-17 15:13:58 +0500

    first commit

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("", views.UserListView.as_view()),
-    path("<pk>", views.UserDetailView.as_view()),
-    path("create/", views.users_create),
-    path('users/', views.UserCreateView.as_view(), name='users_new'), #
+    path("", views.UserListView.as_view(), name="users-list"),
+    path("create/", views.users_create, name="users-create"),
+    path("<pk>", views.UserDetailView.as_view(), name="users-detail"),
+    path("<pk>/update/", views.users_update, name="users-update"),
+    path("<pk>/delete/", views.users_delete, name="users-delete-confirmation"),
 
 ]
